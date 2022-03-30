197981247009cc62660f5b2f7f4d3b3a0133901d
ivai <ismb9@hotmail.com>
2022-03-30 13:45:44 -0600

    termina introduccion a vue

---
+++ 02-bases-vue/bases/app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+    // template: `
+    // <h1>Hola Mundo!!!</h1>
+    // <p> Desde app.js </p>
+    // `
+
+    data() {
+        return {
+            quote: 'Soy Batman',
+            author: 'Bruce Wayne'
+        }
+    },
+    methods: {
+        changeQuote() {
+            
+            console.log( 'Hola Mundo' );
+            this.author = 'Ivan Mendoza';
+            this.capitalize();
+
+        },
+        capitalize() {
+            this.quote = this.quote.toUpperCase();
+        }
+    }
+});
+
+app.mount('.myApp');
