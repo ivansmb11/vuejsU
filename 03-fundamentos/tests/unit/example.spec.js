1ff23ba0a8125232c33b1f225dcf0bb200a1c8bc
ivai <ismb9@hotmail.com>
2022-04-01 02:16:25 -0600

    Seccion 6 avance

--- 03-fundamentos/tests/unit/example.spec.js
+++ 03-fundamentos/tests/unit/example.spec.js
@@ -1,12 +1,21 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+
+describe( 'example component', () => {
+  test( 'Debe de ser mayor a 10', () => {
+
+    //Arreglar
+    const value = 5;
+
+    //Estiulo
+    value = value + 2;
+
+    //Observar el resultado
+    if ( value > 10) {
+      console.log( 'Es mayor a 10' );
+      //TODO: BIEN  -  OK
+    } else {
+      throw `${ value } no es mayor a 10`;
+    }
+    
+  });
+});
